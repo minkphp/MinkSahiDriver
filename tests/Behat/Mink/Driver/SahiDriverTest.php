d6d73253019bb9cd6dd5e5a91d307dc4983c2f67
Christophe Coevoet <stof@notk.org>
2014-05-03 12:52:59 +0200

    Removed some outdated tests
    The issue131 test has already been moved to the upstream driver testsuite.
    the test about prepareXPath was using a private method which has been
    removed.

--- tests/Behat/Mink/Driver/SahiDriverTest.php
+++ tests/Behat/Mink/Driver/SahiDriverTest.php
@@ -18,41 +18,6 @@ class SahiDriverTest extends JavascriptDriverTest
         return new SahiDriver($_SERVER['WEB_FIXTURES_BROWSER'], new Client($connection));
     }
 
-    /**
-     * @group issue131
-     */
-    public function testIssue131()
-    {
-        $this->getSession()->visit($this->pathTo('/issue131.php'));
-        $page = $this->getSession()->getPage();
-
-        $page->selectFieldOption('foobar', 'Gimme some accentués characters');
-    }
-
-    /**
-     * @dataProvider prepareXPathDataProvider
-     */
-    public function testPrepareXPath($expected, $input)
-    {
-        $driver = $this->getSession()->getDriver();
-
-        // Make the method accessible for testing purposes
-        $method = new \ReflectionMethod('Behat\Mink\Driver\SahiDriver', 'prepareXPath');
-        $method->setAccessible(true);
-
-        $this->assertEquals($expected, $method->invokeArgs($driver, array($input)));
-    }
-
-    public function prepareXPathDataProvider()
-    {
-        return array(
-            array('No quotes', 'No quotes'),
-            array("Single quote'", "Single quote'"),
-            array('Double quote\"', 'Double quote"'),
-            array('Multi\nline', "Multi\nline"),
-        );
-    }
-
     public function testIFrame()
     {
         $this->markTestSkipped('Sahi doesn\'t support iFrames switching');
